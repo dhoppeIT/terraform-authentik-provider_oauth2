dhoppeIT/terraform-twingate-resource

chore: Initial commit

examples/default/providers.tf
@@ -1,0 +1,1 @@
+provider "twingate" {}

dhoppeIT/terraform-authentik-provider_oauth2

examples/default/providers.tf
@@ -1,0 +1,1 @@
+provider "authentik" {}